8f9b482d76d0737162ebd56ac9dd893fc1e85fbd
Chad Hutchins <chadhutchins182@users.noreply.github.com>
2020-10-03 14:04:23 -0700

    Remove local and switch to sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+
 set -e
 
 # usage: file_env VAR [DEFAULT]
@@ -6,14 +7,14 @@ set -e
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
 #  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
 file_env() {
-  local var="$1"
-  local fileVar="${var}_FILE"
-  local def="${2:-}"
+  var="$1"
+  fileVar="${var}_FILE"
+  def="${2:-}"
   if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
     echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
     exit 1
   fi
-  local val="$def"
+  val="$def"
   if [ "${!var:-}" ]; then
     val="${!var}"
   elif [ "${!fileVar:-}" ]; then
